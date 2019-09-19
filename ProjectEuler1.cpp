f956bd1dbddb796d945ad2c5a044fade2fb9aa29
Benjamin Ahn <43593958+benjaminsolbiahn@users.noreply.github.com>
2019-09-19 16:58:08 -0700

    Add files via upload

---
+++ ProjectEuler1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(){
+
+int three_total = 0, five_total = 0, total = 0;
+int three_counter = 0, five_counter = 0;
+
+  while(three_counter < 1000){
+    three_counter = three_counter + 3;
+    three_total = three_total + three_counter;
+  }
+
+  while(five_counter < 1000){
+    if((five_counter + 5) % 3 == 0) {
+      five_counter = five_counter + 5;
+    }
+    else{
+      five_counter = five_counter + 5;
+      five_total = five_total + five_counter;
+    }
+  }
+
+  total = five_total + three_total;
+
+  std::cout << "The answer to number 1 of Project Euler is "
+  << total << "!";
+
+  return 0;
+}
